2a2e2b3b9b0626874460062bf3aaac60a3319794
Xin Zheng <xin.zheng.0459@outlook.com>
2025-09-01 20:28:24 +0800

    update configurations

--- src/.config/user.ts
+++ src/.config/user.ts
@@ -1,7 +1,75 @@
 import type { UserConfig } from '~/types'
 
 export const userConfig: Partial<UserConfig> = {
-  // Override the default config here
-  // site: { title: "講評世界" },
-  // seo: { twitter: "@moeyua13" },
+  site: {
+    title: 'XIN ZHENG',
+    subtitle: 'BLOG',
+    author: 'Moeyua',
+    description: 'Blog for daily',
+    website: 'https:/zhengxin.online/',
+    pageSize: 6,
+    socialLinks: [
+      {
+        name: 'github',
+        href: 'https://github.com/zhengxinzx',
+      },
+    ],
+    navLinks: [
+      {
+        name: 'Posts',
+        href: '/',
+      },
+      {
+        name: 'Archive',
+        href: '/archive',
+      },
+      {
+        name: 'Categories',
+        href: '/categories',
+      },
+      {
+        name: 'About',
+        href: '/about',
+      },
+    ],
+    categoryMap: [],
+    footer: [
+      '© %year',
+    ],
+  },
+  appearance: {
+    theme: 'system',
+    locale: 'en-us',
+    colorsLight: {
+      primary: '#2e405b',
+      background: '#ffffff',
+    },
+    colorsDark: {
+      primary: '#FFFFFF',
+      background: '#232222',
+    },
+    fonts: {
+      header:
+        '"HiraMinProN-W6","Source Han Serif CN","Source Han Serif SC","Source Han Serif TC",serif',
+      ui: '"Source Sans Pro","Roboto","Helvetica","Helvetica Neue","Source Han Sans SC","Source Han Sans TC","PingFang SC","PingFang HK","PingFang TC",sans-serif',
+    },
+  },
+  seo: {
+    twitter: '@moeyua13',
+    meta: [],
+    link: [],
+  },
+  rss: {
+    fullText: true,
+  },
+  comment: {
+    // disqus: { shortname: "typography-astro" },
+  },
+  analytics: {
+    googleAnalyticsId: '',
+    umamiAnalyticsId: '',
+  },
+  latex: {
+    katex: false,
+  },
 }
